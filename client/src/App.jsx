197c8f0dda554c8474050c1737b100c535f39f3a
Caeldan Rodrigues <caeldanrodrigues@gmail.com>
2024-02-05 21:04:46 +0530

    set up starter react template

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+
+import { logo } from './assets'
+
+
+export default function App() {
+  return (
+    <h1 className="text-3xl text-amber-700 font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
