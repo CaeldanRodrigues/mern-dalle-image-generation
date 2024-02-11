78531d893377733a48da6395389e412ebcbaf0f6
Caeldan Rodrigues <caeldanrodrigues@gmail.com>
2024-02-11 11:47:36 +0530

    Implemented dalle image generation route

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -8,7 +8,22 @@ const router = express.Router();
 const openai = new OpenAI();
 
 router.get("/", async (req, res) => {
-  res.send("Hello dalle");
+  try {
+    const { prompt } = req.body;
+
+    const aiResponse = await openai.images.generate({
+      model: "dall-e-2", 
+      prompt: prompt, 
+      response_format: 'b64_json' 
+    });
+    const image = aiResponse.data[0].base64;
+
+    res.status(200).json({ photo: image });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error?.response.data.error.message);
+  }
 });
 
 export default router;
