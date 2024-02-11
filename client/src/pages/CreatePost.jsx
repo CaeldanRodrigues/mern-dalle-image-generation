69d1c82041e1eba0734e1c395b7774be27552155
Caeldan Rodrigues <caeldanrodrigues@gmail.com>
2024-02-11 18:25:14 +0530

    Add form submission functionality to CreatePost component

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -43,7 +43,32 @@ export default function CreatePost() {
       alert('Please enter a prompt')
     }
   }
-  const handleSubmit = () => { }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if(form.prompt && form.photo) {
+      setLoading(true);
+
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/posts', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(form),
+        })
+
+        await response.json();
+        navigate('/');
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      alert('Please generate an image first')
+    }
+  }
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value })
