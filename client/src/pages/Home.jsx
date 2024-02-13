6f93a1f458cfd0a84bbfba8d7388cdd69b257953
Caeldan Rodrigues <caeldanrodrigues@gmail.com>
2024-02-13 12:55:04 +0530

    Add search functionality to Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -19,6 +19,8 @@ export default function Home() {
   const [loading, setLoading] = useState(false)
   const [allPosts, setAllPosts] = useState(null)
   const [searchText, setSearchText] = useState('')
+  const [searchResults, setSearchResults] = useState(null)
+  const [searchTimeout, setSearchTimeout] = useState(null)
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -48,6 +50,20 @@ export default function Home() {
     fetchPosts();
   }, [])
 
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout)
+    setSearchText(e.target.value)
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResults = allPosts.filter(post => 
+          post.name.toLowerCase().includes(searchText.toLowerCase()) || post.prompt.toLowerCase().includes(searchText.toLowerCase())
+        )
+        setSearchResults(searchResults)
+      }, 500)
+    )
+  }
+
   return (
     <section className="max-w-7xl mx-auto">
       <div>
@@ -56,7 +72,13 @@ export default function Home() {
       </div>
 
       <div className='mt-16'>
-        <FormField />
+        <FormField
+          LabelName="Search Post"
+          type="text"
+          name="text"
+          placeholder="Search posts"
+          value={searchText}
+          handleChange={handleSearchChange} />
       </div>
 
       <div className="mt-10">
@@ -75,7 +97,7 @@ export default function Home() {
             <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
               { searchText ? (
                 <RenderCards 
-                  data={[]}
+                  data={searchResults}
                   title="No search results found"
                 />
               ) : (
